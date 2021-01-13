1718356f7b7f138e14540ed6e28776e2539546f9
Alex Whittington <alex@southsun.tech>
2021-01-13 13:03:26 -0600

    remove unused code

--- source/app/components/LogWindow.py
+++ source/app/components/LogWindow.py
@@ -6,24 +6,6 @@ from logging.handlers import QueueHandler, QueueListener
 import queue
 
 
-# # https://github.com/beenje/tkinter-logging-text-widget/blob/master/main.py
-# class QueueHandler(logging.Handler):
-#     """Class to send logging records to a queue
-#     It can be used from different threads
-#     The ConsoleUi class polls this queue to display records in a ScrolledText widget
-#     """
-#     # Example from Moshe Kaplan: https://gist.github.com/moshekaplan/c425f861de7bbf28ef06
-#     # (https://stackoverflow.com/questions/13318742/python-logging-to-tkinter-text-widget) is not thread safe!
-#     # See https://stackoverflow.com/questions/43909849/tkinter-python-crashes-on-new-thread-trying-to-log-on-main-thread
-
-#     def __init__(self, log_queue):
-#         super().__init__()
-#         self.log_queue = log_queue
-
-#     # this method is defined but not called -- used internally 
-#     def emit(self, record):
-#         self.log_queue.put(record)
-
 class LogWindow(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
