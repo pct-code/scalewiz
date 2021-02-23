490776f1dbebc81ee7a35a48af9a9985c0af7e01
Alex Whittington <alex@southsun.tech>
2021-02-23 14:17:05 -0600

    add pandas for csv export option

---
+++ pct_scalewiz/models/CSVExport.py
@@ -0,0 +1,14 @@
+import logging
+import tkinter 
+from tkinter import messagebox
+import shutil
+import os
+from datetime import datetime
+import PIL
+import pandas as pd
+
+from models.Project import Project
+
+def export(proj: Project):
+
+    
