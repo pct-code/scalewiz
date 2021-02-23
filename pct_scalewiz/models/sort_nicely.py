cb45d792bb46ad0d68b9c28f0373a93490b7c207
Alex Whittington <alex@southsun.tech>
2021-02-23 09:56:07 -0600

    update sort_nicely

--- pct_scalewiz/models/sort_nicely.py
+++ pct_scalewiz/models/sort_nicely.py
@@ -1,9 +1,10 @@
 # thanks https://blog.codinghorror.com/sorting-for-humans-natural-sort-order/
-
+# but modifying in-place seems rude http://www.compciv.org/guides/python/fundamentals/lists-mutability/
 import re
-def sort_nicely(things):
+def sort_nicely(things) -> list:
     """ Sort the given list in the way that humans expect."""
+    new_list = [thing for thing in things]
     convert = lambda text: int(text) if text.isdigit() else text
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
-    things.sort( key=alphanum_key )
-    return things
+    new_list.sort( key=alphanum_key )
+    return new_list
